037a92942d7cb0298068dd598477e534fcf1cb15
OrthodoxEgo <vgtrofimov@gmail.com>
2021-11-27 22:51:25 +0300

    Сделал тебе фишку при загрузке, охереешь, ни у кого такого нема

--- app/src/main/java/ru/pankratov/trofimov/vgav/MainActivity.kt
+++ app/src/main/java/ru/pankratov/trofimov/vgav/MainActivity.kt
@@ -2,10 +2,16 @@ package ru.pankratov.trofimov.vgav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this, "САШКА БЛЯ РАБОТАЕТ!! А ЁПТА", Toast.LENGTH_LONG).show()
+        super.onBackPressed()
     }
 }
