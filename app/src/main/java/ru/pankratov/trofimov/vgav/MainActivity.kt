2cc31cfe3a416b71d13c8dd00e49a8bbf0518ae5
A.Pankratov <apankrockov2018@gmail.com>
2021-11-28 11:49:23 +0300

    Убрал все наши извращения. Нарисовал разметку Калькулятора (только portrait). Добавил фон

--- app/src/main/java/ru/pankratov/trofimov/vgav/MainActivity.kt
+++ app/src/main/java/ru/pankratov/trofimov/vgav/MainActivity.kt
@@ -4,35 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mBtnStart: Button
+    private lateinit var mTextResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mBtnStart = findViewById(R.id.button_start)
-        mBtnStart.setOnClickListener {
-            startPlayer()
-        }
-    }
+        mTextResult = findViewById(R.id.textView_result)
 
-    private fun startPlayer() {
-        val intent = Intent(this, AudioPlayerActivity::class.java)
-        intent.putExtra(
-            AUDIO_URL,
-            path_song
-        )
-
-        intent.putExtra(
-            IMG_URL,
-            path_img
-        )
-
-        startActivity(intent)
     }
 
     override fun onBackPressed() {
@@ -41,10 +25,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        var path_song = "https://dl3s2.muzofond.fm/aHR0cDovL2YubXAzcG9pc2submV0L21wMy8wMDQvMDQxLzQyNC80MDQxNDI0Lm1wMw=="
-        var path_img = "https://mp3push.com/images/artist/f69afcd63d08124943d657f522d44275.jpg"
 
-        const val AUDIO_URL = "audio_url"
-        const val IMG_URL = "image_url"
     }
 }
