dd5d4b63fcf482c69ba48a1ceaec0dbbbac4d0b1
OrthodoxEgo <vgtrofimov@gmail.com>
2021-11-30 20:56:04 +0300

    Кнопки частично в HashMap. На них можно нажимать, формируя выражение в поле Чайкулятора. И другие истории от журнала "Тёмная ночь, только пули."

--- app/src/main/java/ru/pankratov/trofimov/vgav/Keys.kt
+++ app/src/main/java/ru/pankratov/trofimov/vgav/Keys.kt
@@ -1,26 +1,51 @@
 package ru.pankratov.trofimov.vgav
 
+import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
-class Keys(layout: ConstraintLayout) {
+class Keys(layout: ConstraintLayout){
 
     var layout: ConstraintLayout = layout
-    lateinit var key: MutableMap<String, Button>
-
+    var buttons: HashMap<String, Button> = HashMap()
+    var need_clear_field: Boolean = true
 
     fun main() {
-        key.put("00", layout.findViewById(R.id.button555))
-        key.put("0", layout.findViewById(R.id.button2555))
-        key.put(".", layout.findViewById(R.id.button4555))
-        key.put("=", layout.findViewById(R.id.button5555))
+        buttons.put("00", layout.findViewById(R.id.button555))
+        buttons.put("0", layout.findViewById(R.id.button2555))
+        buttons.put(".", layout.findViewById(R.id.button4555))
+        buttons.put("=", layout.findViewById(R.id.button5555))
 
-        key.get("00")?.setText("ИМЕ")
-        key.get("0")?.setText("НА")
-        key.get(".")?.setText("ГОВ")
-        key.get("=")?.setText("НО")
+        buttons.put("1", layout.findViewById(R.id.button44))
+        buttons.put("2", layout.findViewById(R.id.button244))
+        buttons.put("3", layout.findViewById(R.id.button444))
+        buttons.put("+", layout.findViewById(R.id.button544))
 
+        buttons.get("00")?.setText("ИМЕ")
+        buttons.get("0")?.setText("НА")
+        buttons.get(".")?.setText("ГОВ")
+        buttons.get("=")?.setText("НО")
 
+        for ((key, value) in buttons) {
+            value.setOnClickListener(View.OnClickListener { click(key) })
+        }
+    }
+
+    fun allExpression() {
+
+    }
+
+    fun click(txt: String) {
+        val textView_result: TextView = layout.findViewById(R.id.textView_result)
+
+        if (need_clear_field) {
+            textView_result.setText("")
+            need_clear_field = false
+        }
+
+        var temp_txt = textView_result.getText().toString() + txt
+        textView_result.setText(temp_txt)
     }
 
 
