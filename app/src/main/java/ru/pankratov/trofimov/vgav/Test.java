2d3e53b8eb3087e8bff4ea90e725389e2071ab95
OrthodoxEgo <vgtrofimov@gmail.com>
2021-12-15 11:46:53 +0300

    Test

--- app/src/main/java/ru/pankratov/trofimov/vgav/Test.java
+++ app/src/main/java/ru/pankratov/trofimov/vgav/Test.java
@@ -3,7 +3,9 @@ package ru.pankratov.trofimov.vgav;
 public class Test {
 
     // Скопируй функцию и назови test02, потом отправь в мастер
-    private int test01() {
+    private int test02() {
         return 0;
     }
+
+    private int test01() { return 0; }
 }
